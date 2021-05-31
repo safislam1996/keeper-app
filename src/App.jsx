b553d777d115d9716ed5e8f2f1405bd9b4bc7eaf
safislam1996 <safi.096z@gmail.com>
2021-06-01 00:06:51 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./components/Header.jsx"
+import Footer from "./components/Footer.jsx"
+import Note from "./components/Note.jsx"
+
+function App() {
+
+    return <div>
+
+        <Header />
+        <div className="note">
+
+            <Note /></div>
+
+        <Footer />
+    </div>
+
+}
+
+export default App
